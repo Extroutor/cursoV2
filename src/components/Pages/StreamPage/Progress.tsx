24cbd7a216f7e5a1f0c60c1a0d1233e057c3fcbf
ahmedova_l <ahmedova.l@igit.spb.ru>
2024-11-28 14:37:39 +0300

    fix1

--- src/components/Pages/StreamPage/Progress.tsx
+++ src/components/Pages/StreamPage/Progress.tsx
@@ -6,7 +6,7 @@ import * as stream from "stream"
 import {shortAddress} from "@/utils/shortAddress";
 import {timeLeft} from "@/utils/timeLeft";
 import Clock from "@/icons/clock.tsx";
-import {cancelStream} from "@/blockchain/cancel.ts";
+// import {cancelStream} from "@/blockchain/cancel.ts";
 import {parseDateTime} from "@/utils/dateTimeParser";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Progress = ({data}: { data: any }) => {
