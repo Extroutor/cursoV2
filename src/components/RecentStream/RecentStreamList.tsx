a68a8ac85b594fc39d87acc7d9fe87e7453c5e31
ahmedova_l <ahmedova.l@igit.spb.ru>
2024-11-30 19:18:34 +0300

    test3

--- src/components/RecentStream/RecentStreamList.tsx
+++ src/components/RecentStream/RecentStreamList.tsx
@@ -1,52 +1,56 @@
-import {Box} from '@mui/material'
-import RecentStreamItem from './RecentStreamItem.tsx'
-import useStreams from "@/hooks/useStreams.tsx"
-import {useEffect, useState} from "react"
-import {SubtitleTypography} from "../Typography/SubtitleTypography.tsx"
+    import {Box} from '@mui/material'
+    import RecentStreamItem from './RecentStreamItem.tsx'
+    import useStreams from "@/hooks/useStreams.tsx"
+    import {useEffect, useState} from "react"
+    import {SubtitleTypography} from "../Typography/SubtitleTypography.tsx"
 
-const RecentStreamList = () => {
-    const [groupedStream, setGroupedStream] = useState({})
-    const {streams} = useStreams()
+    type GroupedStream = {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        [date: string]: any[]
+    }
+    const RecentStreamList = () => {
+        const [groupedStream, setGroupedStream] = useState<GroupedStream>({})
+        const {streams} = useStreams()
 
-    useEffect(() => {
-        const groupByDate = () => {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const grouped: any = {}
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            streams.forEach((transaction: any) => {
-                const date = new Date(transaction.start_date).toLocaleDateString('en-GB', { day: 'numeric', month: 'long' })
-                if (!grouped[date]) {
-                    grouped[date] = []
-                }
-                grouped[date].push(transaction)
-            })
-            setGroupedStream(grouped)
-        }
+        useEffect(() => {
+            const groupByDate = () => {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                const grouped: any = {}
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                streams.forEach((transaction: any) => {
+                    const date = new Date(transaction.start_date).toLocaleDateString('en-GB', { day: 'numeric', month: 'long' })
+                    if (!grouped[date]) {
+                        grouped[date] = []
+                    }
+                    grouped[date].push(transaction)
+                })
+                setGroupedStream(grouped)
+            }
 
-        groupByDate()
-    }, [streams])
+            groupByDate()
+        }, [streams])
 
-    return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-        }}>
-            {groupedStream && Object.keys(groupedStream).map((date) => (
-                <Box key={date} marginTop='12px'>
-                    <SubtitleTypography marginBottom='12px'>{date}</SubtitleTypography>
-                    <ul style={{
-                        padding: 0,
-                        margin: 0,
-                    }}>
-                        {/*eslint-disable-next-line @typescript-eslint/no-explicit-any*/}
-                        {groupedStream[date]?.map((stream) => (
-                            <RecentStreamItem key={stream.id} stream={stream}/>
-                        ))}
-                    </ul>
-                </Box>
-            ))}
-        </Box>
-    )
-}
+        return (
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}>
+                {groupedStream && Object.keys(groupedStream).map((date) => (
+                    <Box key={date} marginTop='12px'>
+                        <SubtitleTypography marginBottom='12px'>{date}</SubtitleTypography>
+                        <ul style={{
+                            padding: 0,
+                            margin: 0,
+                        }}>
+                            {/*eslint-disable-next-line @typescript-eslint/no-explicit-any*/}
+                            {groupedStream[date]?.map((stream) => (
+                                <RecentStreamItem key={stream.id} stream={stream}/>
+                            ))}
+                        </ul>
+                    </Box>
+                ))}
+            </Box>
+        )
+    }
 
-export default RecentStreamList
+    export default RecentStreamList
