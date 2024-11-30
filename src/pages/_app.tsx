2da7f9506540f3d152afc974ed52d09ad77d5f7b
ahmedova_l <ahmedova.l@igit.spb.ru>
2024-11-30 22:29:10 +0300

    test connection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,18 +8,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // import StreamDataUpdater from '../components/Stream/StreamsDataUpdater'
 import { createAppKit } from '@reown/appkit/react'
 import { polygonAmoy } from '@reown/appkit/networks'
-import {projectId, wagmiAdapter} from "../../wagmi-config"
+import {metadata, projectId, wagmiAdapter} from "../../wagmi-config"
 
 
 const queryClient = new QueryClient();
 
 // Set up metadata
-const metadata = {
-  name: 'Curso',
-  description: 'AppKit Example - Powered by Curso',
-  url: 'https://curso.finance', // origin must match your domain & subdomain
-  icons: [''], // TODO: Add logo
-};
+// const metadata = {
+//   name: 'Curso',
+//   description: 'AppKit Example - Powered by Curso',
+//   url: 'https://curso.finance', // origin must match your domain & subdomain
+//   icons: [''], // TODO: Add logo
+// };
 
 // Create the modal
 createAppKit({
@@ -30,9 +30,9 @@ createAppKit({
   // defaultNetwork: polygon,
   defaultNetwork: polygonAmoy,
   metadata: metadata,
-  features: {
-    socials: ['google', 'apple'],
-  },
+  // features: {
+  //   socials: ['google', 'apple'],
+  // },
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
--- wagmi-config.ts
+++ wagmi-config.ts
@@ -4,27 +4,28 @@ import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 import { polygonAmoy } from '@reown/appkit/networks';
 import { createPublicClient, http as Vhttp } from 'viem';
 
-const metadata = {
+export const metadata = {
     name: 'Curso',
     description: 'Powered by Curso',
-    url: 'https://curso.finance', // origin must match your domain & subdomain
+    // url: 'https://curso.finance', // origin must match your domain & subdomain
+    url: 'https://curso-v2.vercel.app', // origin must match your domain & subdomain
     icons: [''], // TODO: Add logo
 };
 
 export const projectId = '16f460d50c25a91446f494e58c3999e7'
 // export const infuraId = "28aaa049f7f14880b375f80295d02142"
 
-const connectors: CreateConnectorFn[] = [];
-connectors.push(walletConnect({ projectId: projectId, metadata, showQrModal: false })); // showQrModal must be false
-connectors.push(injected({ shimDisconnect: true }));
-connectors.push(metaMask({ dappMetadata: metadata })); // for mobile connection
+// const connectors: CreateConnectorFn[] = [];
+// connectors.push(walletConnect({ projectId: projectId, metadata, showQrModal: false })); // showQrModal must be false
+// connectors.push(injected({ shimDisconnect: true }));
+// connectors.push(metaMask({ dappMetadata: metadata })); // for mobile connection
 
 export const wagmiAdapter = new WagmiAdapter({
     ssr: true,
     projectId: projectId,
     // networks: [polygon],
     networks: [polygonAmoy],
-    connectors,
+    // connectors,
     // transports: {
     //     // [polygonAmoy.id]: http("https://polygon-amoy.infura.io/v3/28aaa049f7f14880b375f80295d02142"),
     //     [polygonAmoy.id]: fallback([
